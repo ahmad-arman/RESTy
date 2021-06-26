9c4550b2bbc8da0feb3a81453d054eeb3a61b91f
ahmad-ahmad <aa0796780751@gmail.com>
2021-06-26 23:48:10 +0300

    lab29

---
+++ src/help/help.jsx
@@ -0,0 +1,14 @@
+import React from 'react' ; 
+
+const Help = () =>{
+    return (
+        <div>
+             
+              <p>allowing a user to easily interact with APIs in a familiar interface.</p>
+              <p>Simply put , post , put , delete and Bearer authorization URL in form and choose the method you want then click go!</p>
+              <p>You will see the results</p>
+        </div>
+    )
+}
+
+export default Help ; 
